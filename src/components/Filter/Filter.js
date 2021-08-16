edfd7cb635eae5d4eb65c93a392df6d04401413e
Alisa Morenko <Alisa.Morenko@gmail.com>
2021-08-16 13:45:20 +0300

    finish

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,35 +1,3 @@
-// import React from 'react';
-// import { connect } from 'react-redux';
-// import * as phonebookActions from '../../redux/phonebook-actions';
-
-// import styles from './filter.module.css';
-
-// const Filter = ({ value, onFilter }) => {
-//   return (
-//     <label className={styles.label}>
-//       {' '}
-//       Find contacts by name
-//       <input
-//         type="text"
-//         value={value}
-//         onChange={onFilter}
-//         className={styles.input}
-//       ></input>
-//     </label>
-//   );
-// };
-
-// const mapStateToProps = state => ({
-//   contacts: state.phonebook.filter,
-// });
-
-// const mapDispatchToProps = dispatch => ({
-//   onFilter: e => dispatch(phonebookActions.changeFilter(e.target.value)),
-// });
-
-// export default connect(mapStateToProps, mapDispatchToProps)(Filter);
-
-//2.0
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as actions from '../../redux/phonebook-actions';
